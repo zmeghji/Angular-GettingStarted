0acd82951f092416d0089c404f064e322f922694
unknown <zeeshan_meghji@hotmail.com>
2019-04-13 16:35:21 -0400

    completed dependency injection module

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core'
 import { IProduct } from './product';
+import {ProductService} from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -8,6 +9,10 @@ import { IProduct } from './product';
 })
 export class ProductListComponent implements OnInit{
     
+    constructor(productService : ProductService){
+        this._productService = productService;
+    }
+    private _productService : ProductService;
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
@@ -23,10 +28,6 @@ export class ProductListComponent implements OnInit{
         this.filteredProducts = this.listFilter? this.performFilter(this.listFilter) : this.products
     }
 
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
     onRatingClicked(message: string){
         this.pageTitle = 'Product List: ' +message;
     }
@@ -38,40 +39,13 @@ export class ProductListComponent implements OnInit{
         )
     }
     ngOnInit(): void {
+        this.products =  this._productService.getProducts();
+        this.filteredProducts = this.products;
         console.log('In OnInit')
     }
     filteredProducts : IProduct[]=[];
     products:  IProduct[] = [
-        {
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-      },
-      {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      },
-      {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-      }
+       
     ];
 
     toggleImage(): void{
