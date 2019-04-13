d68a56cd79f483063c1f0243730e6023e825ea9c
unknown <zeeshan_meghji@hotmail.com>
2019-04-13 14:21:25 -0400

    completed data-binding course module

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,13 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'pm-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'Angular: Getting Started';
-// }
 
 import {Component} from '@angular/core';
 
